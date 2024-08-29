68f95dde92058802d5fbae81a1ccb946be946fe6
Ulises Noriega <resvmm@gmail.com>
2024-08-28 22:09:08 -0600

    Vista previa de las descargas

--- sources/js/main.js
+++ sources/js/main.js
@@ -7,6 +7,20 @@ const price2 = 26.99;
 const price3 = 50.99;
 const price4 = 90.99;
 
+try {
+    if (localStorage.getItem("moneda") === null) {
+        localStorage.setItem("moneda", "USD");
+    } else {
+        var currency = document.getElementById("currency");
+        if (currency) {
+            currency.value = localStorage.getItem("moneda");
+            convert();
+        }
+    }
+} catch (error) {
+    alert("No se puede guardar la moneda, por favor habilite las cookies.");
+}
+
 function convertCurrency(amount, currency) {
     if (currency === "USD") {
         return amount * usd;
@@ -16,6 +30,7 @@ function convertCurrency(amount, currency) {
         return amount * mxn;
     }
 }
+
 function convert() {
     const currency = document.getElementById("currency").value;
     const result1 = convertCurrency(price1, currency).toFixed(2);
@@ -27,4 +42,5 @@ function convert() {
     document.getElementById("price2").innerHTML = "$" + result2.split(".")[0] + "<small>." + result2.split(".")[1] + "</small>";
     document.getElementById("price3").innerHTML = "$" + result3.split(".")[0] + "<small>." + result3.split(".")[1] + "</small>";
     document.getElementById("price4").innerHTML = "$" + result4.split(".")[0] + "<small>." + result4.split(".")[1] + "</small>";
+    localStorage.setItem("moneda", currency);
 }
